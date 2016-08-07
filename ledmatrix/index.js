51ccd39888bc04e1b4c5d9f037da7d4e3fdbcc34
Vicente Obregon <desarrollogis@gmail.com>
2016-08-07 13:29:54 -0700

    generate

--- ledmatrix/index.js
+++ ledmatrix/index.js
@@ -1,4 +1,14 @@
 
-jQuery.ready(function($) {
-    alert("");
+jQuery(document).ready(function($) {
+    $("table.matrix input").click(function() {
+        var result = "";
+
+        $("table.matrix input").each(function(index, element) {
+            if ((index % 8) == 0) {
+                result = result + ",B";
+            }
+            result = result + ($(element).is(":checked") ? "1" : "0");
+        });
+        $("span.result").text(result);
+    }).get(0).click();
 });
