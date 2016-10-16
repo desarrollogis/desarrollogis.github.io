96aa7e4153ce38f27b9cdeada6dd2e3ee8c7c7d1
Vicente Obregon <desarrollogis@gmail.com>
2016-10-16 02:02:21 -0700

    basic

--- ledmatrix/public/scripts/app.js
+++ ledmatrix/public/scripts/app.js
@@ -4,4 +4,38 @@ angular.module("MainApp", ["ngRoute", "Security"]).config(function($routeProvide
         "templateUrl": "views/home.html"
     });
 }).controller("HomeController", function($rootScope, $scope) {
+    $scope.characters_cb = null;
+    $scope.preDestroy = function() {
+        if ($scope.characters_cb != null) {
+            firebase.database().ref("characters").off("value", $scope.characters_cb);
+            $scope.characters_cb = null;
+        }
+    };
+    $scope.items = {};
+    $scope.auth = firebase.auth().onAuthStateChanged(function(user) {
+        $scope.preDestroy();
+        $scope.items = {};
+        if (user) {
+            $scope.characters_cb = firebase.database().ref("characters").on("value", function(snapshot) {
+                $scope.items = snapshot.val();
+                $scope.$applyAsync();
+            });
+        }
+    });
+    $scope.$on("$destroy", function() {
+        $scope.preDestroy();
+        $scope.auth();
+    });
+    $scope.edit = function(key, value) {
+        $scope.name = key;
+        $scope.size = value.size;
+        $scope.bits = value.bits;
+    };
+    $scope.addBits = function() {
+        var updates = {};
+
+        updates["size"] = $scope.size;
+        updates["bits"] = $scope.bits;
+        firebase.database().ref("characters").child($scope.name).update(updates);
+    };
 });
